9819b270d6613638837f661e0598dcf2f4cc6df7
welfle.pawel <215359@edu.p.lodz.pl>
2020-03-02 18:22:15 +0100

    First Commit

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        List<String> lista = new ArrayList<String>();
+
+        for (int i = 0; i < value.length() + 1; i++) {
+            lista.add(value.substring(i));
+        }
+
+        return lista; //new ArrayList<>(value.length());
     }
 
 }
